429dec02afbd4c9d031b64bf8b045f787c5ef872
Caio Martins <dev.caiomartins@gmail.com>
2023-05-03 12:34:12 +0100

    Fetch posts using client side rendering

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,21 +1,32 @@
 import PostCard from '@/components/PostCard';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+
+type Post = {
+  id: string;
+  title: string;
+  meta: string;
+  slug: string;
+};
 
 const Posts: NextPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    const res = await fetch('/api/posts');
+    const data = await res.json();
+
+    setPosts(data.posts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
     <div className="p-5 max-w-3xl mx-auto space-y-5">
-      <PostCard
-        title="Lorem ipsum dolor sit amet."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio pariatur rem quisquam tempora nulla dolorum impedit cum alias itaque! Ducimus."
-      />
-      <PostCard
-        title="Lorem ipsum dolor sit amet."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio pariatur rem quisquam tempora nulla dolorum impedit cum alias itaque! Ducimus."
-      />
-      <PostCard
-        title="Lorem ipsum dolor sit amet."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio pariatur rem quisquam tempora nulla dolorum impedit cum alias itaque! Ducimus."
-      />
+      {posts.map((post) => (
+        <PostCard title={post.title} description={post.slug} key={post.id} />
+      ))}
     </div>
   );
 };
