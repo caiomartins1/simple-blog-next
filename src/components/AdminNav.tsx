abd07db65978fb1dc4d0b47536a1ceb6b646e733
Caio Martins <dev.caiomartins@gmail.com>
2023-05-08 20:50:24 +0100

    Initial commit new Next Project

---
+++ src/components/AdminNav.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+interface Props {}
+
+const AdminNav: NextPage<Props> = () => {
+  return (
+    <div className="h-screen w-60 bg-gray-200 dark:bg-gray-600">AdminNav</div>
+  );
+};
+export default AdminNav;
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,11 @@
+import AdminNav from '@/components/AdminNav';
+import { NextPage } from 'next';
+
+const AdminPage: NextPage = () => {
+  return (
+    <div className="">
+      <AdminNav />
+    </div>
+  );
+};
+export default AdminPage;
